6990c87de3b4ade9994e0dec5103e35e584eb970
A31Nesta <anthroponesta@gmail.com>
2024-12-23 19:47:52 +0100

    add tiny change to gyro code TODO: Test on phone and implement new system

--- scn/DieController.gd
+++ scn/DieController.gd
@@ -9,8 +9,8 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+# Called every physics update. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
 	# The force is only applied if we're on the ground
 	# Previous versions checked for collisions but it was
 	# slower and it didn't work correctly with many dice
@@ -19,7 +19,7 @@ func _process(delta):
 		
 		# Here we remove the gravity from the accelerometer and make it twice as sensitive to vertical force
 		# The device's sensors give the vertical value as Z so to make it work properly we need to put the Z in the Y's place
-		var forceToApply : Vector3 = (Input.get_accelerometer() - Input.get_gravity()) * Vector3(100, 100, 200) * multDelta
+		var forceToApply : Vector3 = (Input.get_accelerometer()) * Vector3(100, 100, -200) * multDelta
 		forceToApply = Vector3(forceToApply.x, forceToApply.z, forceToApply.y)
 		
 		var torqueToApply : Vector3 = (Input.get_gyroscope()) * 50 * multDelta
